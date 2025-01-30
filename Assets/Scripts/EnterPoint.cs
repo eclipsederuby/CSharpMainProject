1419edd41dec661866d7469747e9d6650afe4a33
𝓡𝓊𝒷𝓎 𝓦𝑒𝓇𝓃𝑒𝓇 <eclipsederuby@gmail.com>
2025-01-30 16:26:32 +0300

    coment

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -11,7 +11,16 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
-    
+
+    // здарова брат
+
+    /*/ 
+     * как дел
+     * че дел
+     * :^)
+    /*/
+
+
     void Start()
     {
         Time.timeScale = _timeScale;
